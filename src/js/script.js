836a2128b0b5a010780fb7bb0c7c90dfdceb856a
alarky <alarky@gmail.com>
2016-07-01 04:30:55 +0900

    support multi device

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,27 @@
-function fmt02(num) {
-    if ( num < 10 ) {
-        return "0" + num;
+$(document).ready(function() {
+    // clock
+    function fmt02(num) {
+        return ("0" + num).slice(-2);
     }
-    return num;
-}
-function update() {
-    var now = new Date();
-    document.getElementById("clockText").innerHTML =
-        fmt02( now.getHours() ) + ":" + fmt02( now.getMinutes() );
-}
-update();
-setInterval('update()',1000);
+
+    function update() {
+        var now = new Date();
+        document.getElementById("clockText").innerHTML =
+            fmt02(now.getHours()) + ":" + fmt02(now.getMinutes());
+    }
+
+    update();
+    setInterval(update, 1000);
+
+    // canvas
+    var canvas = document.createElement("canvas");
+    //canvas.width = window.innerWidth / window.devicePixelRatio;
+    //canvas.height = window.innerHeight / window.devicePixelRatio;
+    canvas.classList.add("box");
+
+    document.getElementById("container").appendChild(canvas);
+
+    var context = canvas.getContext("2d");
+
+    context.fillRect(0, 0, canvas.width, canvas.height);
+});
